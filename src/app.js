c8e431c6aecc3e61d522144730536e27d6a268ed
Lucas Baach <lucasbaach1@gmail.com>
2023-01-20 02:41:10 -0300

    metodo GET com parametro id para pesquisar apenas um time da lista

--- src/app.js
+++ src/app.js
@@ -20,6 +20,17 @@ app.get('/', (req, res) => res.status(200).json({ message: 'Hello World' }));
 
 app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
+app.get('/teams/:id', (req, res) => {
+    const { id } = req.params;
+    const teamID = teams.find((team) => team.id === Number(id));
+
+    if (!teamID) {
+        res.status(404).json({ message: 'Team not found' });
+    }
+
+    return res.status(200).json({ teamID });
+});
+
 app.post('/teams', (req, res) => {
     const newTeam = { ...req.body };
     teams.push(newTeam);
